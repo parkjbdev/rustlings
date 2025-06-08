5297e7ec03793c627ee75ebc942d60b0fe8d5a27
parkjbdev <dev@parkjb.com>
2025-06-08 15:04:34 +0900

    solve: quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,11 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity: usize) -> u32 {
+    let price: u32 = if quantity > 40 { 1 } else { 2 };
+
+    quantity as u32 * price
+}
 
 fn main() {
     // You can optionally experiment here.
