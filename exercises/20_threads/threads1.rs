6e642302cd726b20ed04385c7e3cdf2f1a9362b1
parkjbdev <dev@parkjb.com>
2025-06-14 13:58:06 +0900

    solve: 20_threads

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -24,6 +24,10 @@ fn main() {
     for handle in handles {
         // TODO: Collect the results of all threads into the `results` vector.
         // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        results.push(match handle.join() {
+            Ok(val) => val,
+            Err(_) => panic!("Thread panicked"),
+        });
     }
 
     if results.len() != 10 {
